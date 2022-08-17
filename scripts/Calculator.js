a2bcf69fc8a47e251a418548349b86f53cae9da5
Abed Fetrat <abedfetrat@gmail.com>
2022-08-17 11:41:29 +0200

    fix bugs

--- scripts/Calculator.js
+++ scripts/Calculator.js
@@ -4,18 +4,28 @@ export default class Calculator {
     constructor(displayElement) {
         this.displayElement = displayElement;
         this.displayText = '0';
-        this.operand = '0';
+        this.operands = ['0'];
         this.evaluator = new Evaluator();
     }
 
     delete() {
-        this.displayText = this.displayText.substring(this.displayText.length - 1, -1);
+        const lastChar = this.displayText[this.displayText.length - 1];
+        const operandIndex = this.operands.length - 1;
+        if (/[0-9\.]/.test(lastChar)) {
+            this.operands[operandIndex] = this.operands[operandIndex].slice(0, -1);
+        }
+
+        if (this.operands[operandIndex].length === 0 && this.operands.length > 1) {
+            this.operands.pop();
+        }
+
+        this.displayText = this.displayText.slice(0, -1);
         this.updateDisplay();
     }
 
     reset() {
         this.displayText = '0';
-        this.operand = '0';
+        this.operands = ['0'];
         this.updateDisplay();
     }
 
@@ -27,6 +37,8 @@ export default class Calculator {
         }
 
         this.displayText = this.evaluator.eval(this.displayText).toString();
+        // Update operand with the result 
+        this.operands = [this.displayText];
         this.updateDisplay();
     }
 
@@ -38,11 +50,12 @@ export default class Calculator {
         // Prevents multiple decimals after one another
         if (last === key.dataset.value) return;
 
-        // Prevents multiple decimals in a single operator
-        if (this.operand.includes(key.dataset.value)) return;
+        // Prevents multiple decimals in a single operand
+        const operand = this.operands[this.operands.length - 1];
+        if (operand.includes(key.dataset.value)) return;
 
-        // Update operand with the value
-        this.operand += key.dataset.value;
+        // Updates operand with the value or returns if max allowed digits is reached
+        if (!this.updateOperand(key.dataset.value)) return;
 
         // Update display text with the value
         this.displayText += key.dataset.value;
@@ -50,9 +63,6 @@ export default class Calculator {
     }
 
     operator(key) {
-        // Reset operand in order to track the next operand
-        this.operand = '';
-
         const last = this.displayText[this.displayText.length - 1];
         // Prevents adding operator before first operand and immediately after decimal character
         if (!/[0-9]/.test(last)) return;
@@ -60,22 +70,32 @@ export default class Calculator {
         // Prevents multiple operators after one another
         if (/[+\-/*]/.test(last)) return;
 
+        // Set up empty operand to be updated with value
+        this.operands.push('');
+
         // Update display text with the value
         this.displayText += key.dataset.value;
         this.updateDisplay();
     }
 
     number(key) {
-        // Update operand with the value
-        this.operand += key.dataset.value;
-
-        // Update display text with the value
         if (this.displayText === '0') {
+            // Initially when display and operand is set to zero, replace zero with the number value
+            this.operands[this.operands.length - 1] = key.dataset.value;
             this.displayText = key.dataset.value;
         } else {
+            // Updates operand with the number value or returns if max allowed digits is reached
+            if (!this.updateOperand(key.dataset.value)) return;
             this.displayText += key.dataset.value;
         }
         this.updateDisplay();
+    }
+
+    updateOperand(value) {
+        const last = this.operands.length - 1;
+        if (this.operands[last].length === 15) return false;
+        this.operands[last] += value;
+        return true;
     }
 
     updateDisplay() {
